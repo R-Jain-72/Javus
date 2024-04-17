679ea13ac193cf3b0e780f2d0b5ebe8148d702db
Rishabh J <rishabhj1470@gmail.com>
2024-04-17 11:33:32 +0530

    update: road design completed

--- road.js
+++ road.js
@@ -17,15 +17,35 @@ class Road{
         ctx.lineWidth = 5;//thick line width
         ctx.strokeStyle = "white";
 
-        ctx.beginPath();
-        //drawing vertical line on the left side of the road
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
+        for(let i = 0; i <= this.laneCount; i++){
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
 
+            //add dashes to the middle of the line
+            if(i > 0 && i < this.laneCount){
+                ctx.setLineDash([20, 20]);
+            }
+            else{
+                ctx.setLineDash([]);
+            }
+        
+            ctx.beginPath();
+            /*
+            //drawing vertical line on the left side of the road
+            ctx.moveTo(this.left, this.top);
+            ctx.lineTo(this.left, this.bottom);*/
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }   
+        /*
         //drawing vertical line on the right side of the road
         ctx.moveTo(this.right, this.top);
         ctx.lineTo(this.right, this.bottom);
-        ctx.stroke();
+        ctx.stroke();*/
     }
 }
+
---
+++ utils.js
@@ -0,0 +1,3 @@
+function lerp(A,B,t){
+    return A + (B-A)*t;//calculates a point between two other points based on a scalar value between 0 and 1.
+}

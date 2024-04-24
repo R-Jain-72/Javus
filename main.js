0d240f3e3b9d9c531c19e8afee92403456b2cecc
Rishabh J <rishabhj1470@gmail.com>
2024-04-24 23:43:01 +0530

    More Road Features Added

--- main.js
+++ main.js
@@ -14,7 +14,15 @@ animate();
 function animate(){
     car.update();
     canvas.height=window.innerHeight;
+
+    //looks like a camera is following the car
+    ctx.save();
+    //ctx.translate(0,-car.y+canvas.height*0.5);//canvas height * 0.5 makes it centered
+    ctx.translate(0,-car.y+canvas.height*0.7);
+
     road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);//calls the animate function repeatedly which gives the illusion of the car moving
 }

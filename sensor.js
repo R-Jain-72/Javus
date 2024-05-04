bf600a9d49b04db37792c0fa899e653362189cdd
Rishabh J <rishabhj1470@gmail.com>
2024-05-04 23:23:08 +0530

    sensors

--- sensor.js
+++ sensor.js
@@ -19,26 +19,13 @@ class Sensor{
         }
     }
 
-    #getReadings(ray,roadBorders){
-        let touches = [];
-        for(let i = 0; i < roadBorders.length; i++){
-            const touch = getIntersection(
-                ray[0],
-                ray[1],
-                roadBorders[i][0],
-                roadBorders[i][1]
-            );
-
-            if(touch){
-                touches.push(touch);
-            }
-        }
+   
 
         if(touches.length.length==0){
             return null;
         }
         else{
-            
+
         }
     }
 
